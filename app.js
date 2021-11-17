a07d170c3fce96990c9ab24543c192f11f6ea089
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-16 22:52:30 -0500

    update routers

--- app.js
+++ app.js
@@ -4,22 +4,34 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sqlite3 = require('sqlite3').verbose();
 
-const indexRouter = require('.routes/index');
-const postsRouter = require('.routes/posts');
+const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
 
 const app = express();
 
-let db =new sqlite3.Database("./db/test.db", (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the File database.');
-});
+// let db =new sqlite3.Database("./db/test.db", (err) => {
+//     if (err) {
+//         return console.error(err.message);
+//     }
+//     console.log('Connected to the File database.');
+// });
 
-db.close((err) => {
-    if (err) {
-        return console.error(err.message);
-    }
+// db.close((err) => {
+//     if (err) {
+//         return console.error(err.message);
+//     }
 
-    console.log('Close the database connection.');
-});
+//     console.log('Close the database connection.');
+// });
+
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/posts', postsRouter);
+app.use('/', indexRouter);
+
+module.exports = app;
