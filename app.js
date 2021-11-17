4ab898b35b73a644e503e43d36bb8b98780fc0d0
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-16 22:18:13 -0500

    update package.json

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sqlite3 = require('sqlite3').verbose();
+
+const indexRouter = require('.routes/index');
+const postsRouter = require('.routes/posts');
+
+const app = express();
+
+let db =new sqlite3.Database(":memory", (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the in-memory Sqlite database.');
+});
+
+db.close((err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Close the database connection.');
+});
